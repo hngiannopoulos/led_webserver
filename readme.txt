7a896f6d5bc4f5e999fe96eaa7da5f9299aaa69b
Hristos Giannopoulos <hristos@outlook.com>
2014-07-28 00:39:41 +0000

    Gets basic webserver up and running

---
+++ readme.txt
@@ -0,0 +1,4 @@
+To get the script to work from php you need to run:
+
+sudo chmod 666 /dev/ttyAMA0
+
--- www/test.php
+++ www/test.php
@@ -1,11 +1,18 @@
 <html>
-<body>
-Testing
-<?php
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php
+	$command="/var/www/script.sh ". $_POST[table_command]. $_POST[message];
+	echo $command;
+	$output = shell_exec($command);
+	echo $output;
+	 header('Location: http://ledtable.dnsdynamic.com');
+	//echo exec("stty -F  /dev/ttyAMA0 115200");
+	//echo exec("echo C > /dev/ttyAMA0");
+	//echo "HELLO";
 
-$command="/scripts/script.sh ", $_POST[table_command], $_POST[message]
-echo "TEST" <br>
-echo $command
-?>
-</body>
+ ?>
+ </body>
 </html>
